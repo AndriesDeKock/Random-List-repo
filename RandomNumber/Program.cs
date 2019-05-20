da1339568a527c8b174059c5575647e061687ba0
AndriesDeKock <andries@snowblu.com>
2019-05-20 15:23:24 +0200

    Add project files.

---
+++ RandomNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomNumber
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<UserModel> people = new List<UserModel> {
+                new UserModel{ FirstName = "John"},
+                new UserModel{ FirstName = "Peter"},
+                new UserModel{ FirstName = "Sarah"},
+                new UserModel{ FirstName = "Jessica"},
+                new UserModel{ FirstName = "Joan"},
+                new UserModel{ FirstName = "Sammie"},
+                new UserModel{ FirstName = "George"},
+                new UserModel{ FirstName = "Harry"},
+            };
+
+            people.Shuffle();
+
+            foreach (var p in people)
+            {
+                Console.WriteLine(p.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+
+        public class UserModel
+        {
+            public string FirstName { get; set; }
+        }
+    }
+
+}
---
+++ RandomNumber/Shuffle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RandomNumber
+{
+    public static class ShuffleClass
+    {
+        public static void Shuffle<T>(this IList<T> List) {
+
+            RNGCryptoServiceProvider provider = new RNGCryptoServiceProvider();
+            int ListCount = List.Count;
+
+            while (ListCount > 1)
+            {
+                byte[] box = new byte[1];
+
+                do
+                {
+                    provider.GetBytes(box);
+
+                } while (box[0] > ListCount * (Byte.MaxValue / ListCount));
+
+                int k = (box[0] % ListCount);
+                ListCount--;
+
+                T value = List[k];
+                
+                List[k] = List[ListCount];
+                List[ListCount] = value;
+                
+            }
+
+        }
+    }
+}
